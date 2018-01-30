05c3ddccf4ba3e41058a2381f4468efe5c9434ee
Kimberly Huynh and Logan Tanous <kimberly+logan@>
2018-01-30 10:57:47 -0800

    add little guy background images

--- js/scripts2.js
+++ js/scripts2.js
@@ -41,17 +41,28 @@ var mapLayout = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'+
 'x____[]___________8_____________[]_______________x'+
 'xxxxxxxxxxxxxxxxx===xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
 
-function Player(xCoord, yCoord) {
+function Player(xCoord, yCoord, facing) {
   this.xCoord = xCoord;
   this.yCoord = yCoord;
   this.currentSpot = 2018;
   this.nextSpot = "";
+  this.facing = "up";
 }
 
-var player1 = new Player(18,40);
+var player1 = new Player(18,40, "down");
+
+Player.prototype.interact = function(interactWith) {
+  if (interactWith == "talk") {
+    //check if spaces near are interactable
+
+    console.log(this.facing);
+  }
+}
 
 Player.prototype.move = function(way) {
   //collision detection
+
+  this.facing = way;
 
   this.currentSpot = passConvertCoordinates(this.xCoord,this.yCoord);
   if (way == "left") {
@@ -60,15 +71,15 @@ Player.prototype.move = function(way) {
   if (way == "up") {
     this.nextSpot = passConvertCoordinates(this.xCoord,this.yCoord-1);
   }
-  if (way == "right") {this.nextSpot = passConvertCoordinates(
-    this.xCoord+1,this.yCoord);
+  if (way == "right") {
+    this.nextSpot = passConvertCoordinates(this.xCoord+1,this.yCoord);
   }
-  if (way == "down") {this.nextSpot = passConvertCoordinates(
-    this.xCoord,this.yCoord+1);
+  if (way == "down") {
+    this.nextSpot = passConvertCoordinates(this.xCoord,this.yCoord+1);
   }
 
   if (mapLayout.charAt(this.nextSpot) !== "_") {
-    if (mapLayout.charAt(this.nextSpot) == "&") {alert("hello")}
+    if (mapLayout.charAt(this.nextSpot) == "&") {console.log("hello")}
     else {
     this.nextSpot = "";
     }
@@ -76,15 +87,19 @@ Player.prototype.move = function(way) {
   else {
     if (way == "left") {
       this.xCoord = this.xCoord-1;
+      //console.log("left");
     }
     if (way == "up") {
       this.yCoord = this.yCoord-1;
+      //console.log("up");
     }
     if (way == "right") {
       this.xCoord = this.xCoord+1;
+      //console.log("right");
     }
     if (way == "down") {
       this.yCoord = this.yCoord+1;
+      //console.log("down");
     }
     mapLayout = mapLayout.replaceAt(this.currentSpot, "_");
     mapLayout = mapLayout.replaceAt(this.nextSpot, "8");
@@ -99,8 +114,6 @@ String.prototype.replaceAt=function(index, replacement) {
 }
 
 function drawScreen() {
-
-
 
   var map1Layout = mapLayout;
 
@@ -121,8 +134,15 @@ function drawScreen() {
   for (var i = 0; i < 42; i++) {
     //row
     for (var x = 0; x < 50; x++) {
-      $("#display").append("<span class='"+map1Layout.charAt(character)+" pixels'>"+"</span>");
-       character++;
+
+      if (map1Layout.charAt(character) == "p") {
+        $("#display").append("<span class='"+map1Layout.charAt(character)+" "+player1.facing+" pixels'>"+"</span>");
+        console.log(player1.facing);
+      }
+      else {
+        $("#display").append("<span class='"+map1Layout.charAt(character)+" pixels'>"+"</span>");
+       }
+        character++;
     }
     $("#display").append("<br>");
   }
@@ -147,24 +167,52 @@ Game.run = function() {
   drawScreen();
 };
 
-$(document).keydown(function(e){
-    if (e.keyCode == 37) {
-       player1.move("left");
-       return false;
-    }
-    if (e.keyCode == 38) {
-       player1.move("up");
-       return false;
-    }
-    if (e.keyCode == 39) {
-       player1.move("right");
-       return false;
-    }
-    if (e.keyCode == 40) {
-       player1.move("down");
-       return false;
-    }
-});
+var change = {
+  37: {
+    left: "-=1"
+  },
+
+  38: {
+    top: "-=1"
+  },
+
+  39: {
+    left: "+=1"
+  },
+
+  40: {
+    top: "+=1"
+  },
+}
+$(document).on({
+  keydown: keydown,
+  keyup: keyup
+})
+
+var movement = []
+
+function keydown(e) {
+  var key = e.which
+  var animation = change[key];
+  if (!movement[key]) { // watch out for repeating keys!
+      movement[key] = setInterval(keepGoing)
+
+  }
+  //  console.log("down", key, movement[key])
+  function keepGoing() {
+    //console.log(animation)
+    if (key == 37) {player1.move("left");}
+    if (key == 38) {player1.move("up");}
+    if (key == 39) {player1.move("right");}
+    if (key == 40) {player1.move("down");}
+    if (key == 84) {player1.interact("talk");}
+  }
+}
+
+function keyup(e) {
+  var key = e.which
+  movement[key] = clearInterval(movement[key])
+}
 
 $(document).ready(function() {
 
